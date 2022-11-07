501c1d30d38ab809033ada67bd88e3e74063c60b
emoltz <es5541@nyu.edu>
2022-11-06 20:59:37 -0700

    bootstrap modal

--- TurnPageRoot/static/js/script.js
+++ TurnPageRoot/static/js/script.js
@@ -170,3 +170,25 @@ if (loginFields) {
     loginFields[1].placeholder = 'Username'
     loginFields[2].placeholder = 'Password'
 }
+
+
+$("#window").click(function () {
+  let sa = $(this).text();
+  $(this).text("");
+  $(this).addClass("ball");
+  setTimeout(function () {
+    $("#modal").fadeIn();
+    $("#close").css("width", "30px");
+    $("#close").css("height", "30px");
+    setTimeout(function () {
+      $("#window").removeClass("ball");
+      $("#window").text(sa);
+    }, 500);
+  }, 800);
+});
+
+$("#close").click(function () {
+  $("#modal").fadeOut();
+  $("#close").css("width", "0px");
+  $("#close").css("height", "0px");
+});
