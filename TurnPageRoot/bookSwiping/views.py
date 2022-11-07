06ad922b3d59f7ed17aae5c12df9438ad1801360
emoltz <es5541@nyu.edu>
2022-11-06 21:09:53 -0700

    modal workin

--- TurnPageRoot/bookSwiping/views.py
+++ TurnPageRoot/bookSwiping/views.py
@@ -9,7 +9,6 @@ class BookshelfView(LoginRequiredMixin, TemplateView):
     model = Book
     template_name = 'bookSwiping/bookshelf.html'
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         random_items = random.sample(list(self.model.objects.all()), 10)
@@ -23,26 +22,35 @@ class HomeView(ListView):
     model = Book
     context_object_name = "books"
     template_name = "bookSwiping/home.html"
+    random_items = []
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         items = list(self.model.objects.all())
         # change to how many random items you want
-        random_items = random.sample(items, 15)
+        self.random_items = random.sample(items, 15)
         # creates a list of books, random for now, from the database
-        context["book01"] = random_items[0]
-        context["book02"] = random_items[1]
-        context["book03"] = random_items[2]
-        context["book04"] = random_items[3]
-        context["book05"] = random_items[4]
-        context["book06"] = random_items[5]
-        context["book07"] = random_items[6]
-        context["book08"] = random_items[7]
-        context["book09"] = random_items[8]
-        context["book10"] = random_items[9]
-        context["book11"] = random_items[10]
-        context["book12"] = random_items[11]
-        context["book13"] = random_items[12]
-        context["book14"] = random_items[13]
-        context["book15"] = random_items[14]
+        context["book01"] = self.random_items[0]
+        context["book02"] = self.random_items[1]
+        context["book03"] = self.random_items[2]
+        context["book04"] = self.random_items[3]
+        context["book05"] = self.random_items[4]
+        context["book06"] = self.random_items[5]
+        context["book07"] = self.random_items[6]
+        context["book08"] = self.random_items[7]
+        context["book09"] = self.random_items[8]
+        context["book10"] = self.random_items[9]
+        context["book11"] = self.random_items[10]
+        context["book12"] = self.random_items[11]
+        context["book13"] = self.random_items[12]
+        context["book14"] = self.random_items[13]
+        context["book15"] = self.random_items[14]
+
+        context["currentBook"] = self.getCurrentBook()
+
         return context
+
+    def getCurrentBook(self):
+        # this will be changed to get the current book that is on top
+        pass
+
